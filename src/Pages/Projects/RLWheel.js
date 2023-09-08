5d4a1b0d23b65b952048a4059628215f142b2c2e
Sean Ghaeli <seanghaeli@gmail.com>
2023-09-07 17:36:51 -0700

    Make various mobile optimizations

--- src/Pages/Projects/RLWheel.js
+++ src/Pages/Projects/RLWheel.js
@@ -27,7 +27,7 @@ const RLWheel = () => {
           <Grid container spacing={2} alignItems="center" justifyContent="center">
             <Grid item xs={12} md={8}>
               <Box sx={{ my: 1 }}>
-                <Typography variant="body1" style={{ color: 'rgba(0, 0, 0, 1)', textAlign: 'left', fontSize: 'inherit'}}>
+                <Typography variant="body1" style={{ color: 'rgba(0, 0, 0, 1)', textAlign: 'left', fontSize: 'inherit' }}>
                   The complexities and inconsistencies of physical systems present challenges for reinforcement learning applications.
                   To bridge this gap, the project endeavors to create self-resetting, autonomous platforms for "sim2real" training,
                   ultimately enhancing the application of simulation-trained models to real-world scenarios.
@@ -46,62 +46,67 @@ const RLWheel = () => {
                   alt="Everyday Robotics Grasping Robot Sim + Real Training Results"
                   style={{ height: '100%', objectFit: 'contain' }}
                 />
-                {/* Figure label */}
                 <Typography
                   variant="subtitle2"
                   component="p"
-                  style={{ fontSize: '1vw', marginTop: '0.5rem', color: 'rgba(0, 0, 0, 0.8)' }}
+                  style={{
+                    fontSize: {
+                      xs: '3.5vw',
+                      sm: '1vw'
+                    }, 
+                    marginTop: '0.5rem', 
+                    color: 'rgba(0, 0, 0, 0.8)' }}
                 >
-                  Figure 1: Everyday Robots conducted experiments to train grasping robots using a combination of simulation and real-world data, improving the performance of the robots and informing the development of an optimized controller.
-                </Typography>
-              </div>
-            </Grid>
+                Figure 1: Everyday Robots conducted experiments to train grasping robots using a combination of simulation and real-world data, improving the performance of the robots and informing the development of an optimized controller.
+              </Typography>
+            </div>
           </Grid>
+          </Grid >
         ),
       },
-      {
-        title: 'System',
-        content: (
-          <>
-            <Grid container spacing={2} alignItems="flex-start">
-              <Grid item xs={12} md={6}>
-                <CardMedia
-                  component="img"
-                  image="rlwheel-system-diagram.png"
-                  alt="System Diagram"
-                  style={{ height: '100%', objectFit: 'contain' }}
-                />
-                <Box sx={{ position: 'relative', width: '100%', height: 0, paddingTop: '56.25%' }}>
-                  <iframe
-                    title="YouTube Video"
-                    width="100%"
-                    height="100%"
-                    src="https://www.youtube.com/embed/lUwQqfhzdlQ"
-                    frameBorder="0"
-                    allowFullScreen
-                    style={{ position: 'absolute', top: 0, left: 0 }}
-                  />
-                </Box>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <CardMedia
-                  component="img"
-                  image="rlwheel-system.png"
-                  alt="Systme objective"
-                  style={{ height: '100%', objectFit: 'contain' }}
-                />
-              </Grid>
-            </Grid>
-            <Typography variant="body2" style={{ marginTop: '1rem', textAlign: 'left', fontSize: 'inherit' }}>
-              Learn more in the full <RouterLink component={RouterLink} to="/rlwheelreport.pdf" target="_blank" rel="noopener noreferrer"> Technical Report</RouterLink>.
-            </Typography>
-          </>
-        ),
+{
+  title: 'System',
+    content: (
+      <>
+        <Grid container spacing={2} alignItems="flex-start">
+          <Grid item xs={12} md={6}>
+            <CardMedia
+              component="img"
+              image="rlwheel-system-diagram.png"
+              alt="System Diagram"
+              style={{ height: '100%', objectFit: 'contain' }}
+            />
+            <Box sx={{ position: 'relative', width: '100%', height: 0, paddingTop: '56.25%' }}>
+              <iframe
+                title="YouTube Video"
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/lUwQqfhzdlQ"
+                frameBorder="0"
+                allowFullScreen
+                style={{ position: 'absolute', top: 0, left: 0 }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <CardMedia
+              component="img"
+              image="rlwheel-system.png"
+              alt="Systme objective"
+              style={{ height: '100%', objectFit: 'contain' }}
+            />
+          </Grid>
+        </Grid>
+        <Typography variant="body2" style={{ marginTop: '1rem', textAlign: 'left', fontSize: 'inherit' }}>
+          Learn more in the full <RouterLink component={RouterLink} to="/rlwheelreport.pdf" target="_blank" rel="noopener noreferrer"> Technical Report</RouterLink>.
+        </Typography>
+      </>
+    ),
       },
     ],
   };
 
-  return <ProjectPage project={project} />;
+return <ProjectPage project={project} />;
 };
 
 export default RLWheel;
