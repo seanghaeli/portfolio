0410a4092e2526ed25de82d60ed80eb1a430cab5
Sean Ghaeli <seanghaeli@Seans-MacBook-Air.local>
2025-01-27 14:39:32 -0800

    Add route to rainbow lectures

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ import Videos from './Pages/Projects/Videos';
 import RCL from './Pages/Projects/RCL'
 import { HelmetProvider } from 'react-helmet-async';
 
+function RainbowRedirect() {
+  window.location.href = "https://www.youtube.com/watch?v=g7rV8b0Tb2k&list=PLjYAr6I4xGWyMt2disjClb3SARbW1Tb17&ab_channel=SeanGhaeli"
+  return null;
+}
+
 function App() {
   return (
     <HelmetProvider>
@@ -29,7 +34,7 @@ function App() {
         sx={{
           backgroundColor: '#F7F7F7',
           padding: '2% 4%',
-          minHeight: '100vh', // Update to '100vh'
+          minHeight: '100vh', // Update to '100vh
           display: 'flex',
           flexDirection: 'column',
         }}
@@ -45,6 +50,7 @@ function App() {
           <Route path="/lectures" element={<Videos />} />
           <Route path="/quickestascent.pdf" element={<QuickestAscent />} />
           <Route path="/isingmodel.pdf" element={<IsingModel />} />
+          <Route path="/rainbow" element={<RainbowRedirect />} />
         </Routes>
       </Box>
     </Router>
